ff30a1850a4ac12c91fa6c83ab5000232f344cf1
Vel Wu <ericwu102@yahoo.com.tw>
2020-04-28 18:41:00 -0500

    Another attempt of converting data explorations so far into py~

--- central_analysis.py
+++ central_analysis.py
@@ -11,25 +11,30 @@ import scipy.cluster
 
 from pandas.io.json import json_normalize
 
-df = pd.read_excel('Un.xlsx')
-#Data Cleaning
-df['State'] = df['State Abb.'].map(lambda x:x. split(',')[1])
-df.set_index('State', drop=True, append=False, inplace=True, verify_integrity=False)
-State_total = df.groupby('State').sum()
-State_total.drop(['2008_(%)', '2009_(%)','2010_(%)','2011_(%)','2012_(%)','2013_(%)','2014_(%)','2015_(%)','2016_(%)','2017_(%)','2018_(%)'], axis=1,inplace= True )
-State_total.drop(['Code'], axis=1,inplace= True)
 
-# Find State_total['20XX_Percentage'] = State_total["20XX_Unemployed"] / State_total["20XX_Labor"]*100
-for i in range(11):
-    State_total[f'{2008+i}'] = State_total.iloc[0:,3*i+2] / State_total.iloc[0:,3*i]*100
-State_total_percentages_only = State_total.iloc[0:, -11:]
-# The same table, but flipped around
-State_total_percentages_only_flipped = State_total_percentages_only.transpose()
-State_total_percentages_only_flipped = State_total_percentages_only_flipped.rename({'State': 'Years'}, axis=1)
-State_total_percentages_only_flipped.index.names = ['Years']
-State_total_percentages_only_flipped.plot(figsize=(16, 20))
-plt.show()
+def main():
+    df = pd.read_excel('Un.xlsx')
+    # Data Cleaning
+    df['State'] = df['State Abb.'].map(lambda x: x.split(',')[1])
+    df.set_index('State', drop=True, append=False, inplace=True, verify_integrity=False)
+    State_total = df.groupby('State').sum()
+    State_total.drop(
+        ['2008_(%)', '2009_(%)', '2010_(%)', '2011_(%)', '2012_(%)', '2013_(%)', '2014_(%)', '2015_(%)', '2016_(%)',
+         '2017_(%)', '2018_(%)'], axis=1, inplace=True)
+    State_total.drop(['Code'], axis=1, inplace=True)
 
+    # Find State_total['20XX_Percentage'] = State_total["20XX_Unemployed"] / State_total["20XX_Labor"]*100
+    for i in range(11):
+        State_total[f'{2008 + i}'] = State_total.iloc[0:, 3 * i + 2] / State_total.iloc[0:, 3 * i] * 100
+    State_total_percentages_only = State_total.iloc[0:, -11:]
+    # The same table, but flipped around
+    State_total_percentages_only_flipped = State_total_percentages_only.transpose()
+    State_total_percentages_only_flipped = State_total_percentages_only_flipped.rename({'State': 'Years'}, axis=1)
+    State_total_percentages_only_flipped.index.names = ['Years']
+    State_total_percentages_only_flipped.plot(figsize=(16, 20))
+    plt.show()
+
+main()
 ### PART 2: hc_coverage |  To-be-converted to .py
 """
 # Read in the data and drop Excel sheet comment blocks
